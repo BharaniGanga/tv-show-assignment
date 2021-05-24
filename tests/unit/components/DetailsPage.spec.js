8a518da0f9d9e879307b795e79c3df3f2d9fce4f
BharaniGanga <74890554+BharaniGanga@users.noreply.github.com>
2021-05-25 00:30:29 +0530

    commit

--- tests/unit/components/DetailsPage.spec.js
+++ tests/unit/components/DetailsPage.spec.js
@@ -5,16 +5,6 @@ import DetailsPage from '@/components/DetailsPage.vue';
 import VueRouter from 'vue-router';
 import { routes } from '@/router/index';
 
-
- /*  jest.mock('@/Service/api', () => ({
-    getShowCasts: () => {
-    return Promise.resolve({
-      data: {  "id": 2, "name": "Under the Domesss", "rating": { "average": 6.6 },
-      "image": { "medium": "https://static.tvmaze.com/uploads/images/medium_portrait/81/202627.jpg" }, "genres": ["Drama", "Science-Fiction", "Thriller"]
-     }
-    })
-  },
-}));   */
 const localVue = createLocalVue();
 
 localVue.use(VueRouter);
@@ -29,23 +19,8 @@ describe('DetailsPage.vue', () => {
     wrapper = shallowMount(DetailsPage, {
       localVue,
       router,
-      data(){
-        return{
-          showDetails: { "id": 2, "name": "Under the Dome", "rating": { "average": 6.6 },
-          "image": { "medium": "https://static.tvmaze.com/uploads/images/medium_portrait/81/202627.jpg" }, "genres": ["Drama", "Science-Fiction", "Thriller"]
-        },
-         /*  tvShowCast: [{
-          "id": 2, "name": "Under the Dome", "rating": { "average": 6.6 },
-          "image": { "medium": "https://static.tvmaze.com/uploads/images/medium_portrait/81/202627.jpg" }, "genres": ["Drama", "Science-Fiction", "Thriller"]
-        
-        }],  */
-        showSeason: [{"id": 3,
-          "url": "https://www.tvmaze.com/seasons/3/person-of-interest-season-1",
-          "number": 1,
-          "image": {
-            "medium": "https://static.tvmaze.com/uploads/images/medium_portrait/24/60864.jpg",
-          }}]
-        }
+      propsData:{
+        shows:1
       }
 
     });
@@ -56,39 +31,36 @@ describe('DetailsPage.vue', () => {
   });
    it('is a Vue instance', () => {
     expect(wrapper.isVueInstance).toBeTruthy();
-    console.log(wrapper.html())
-  }); 
- /*  it('should find p tag', () => {
-    let wrappers;
-    wrappers = shallowMount(DetailsPage, {
-      localVue,
-      router,
-      data(){
-        return{
-          showDetails: {  "id": 2, "name": "Under the Dome", "rating": { "average": 6.6 },
-          "image": { "medium": "https://static.tvmaze.com/uploads/images/medium_portrait/81/202627.jpg" }, "genres": ["Drama", "Science-Fiction", "Thriller"]
-        }
-        }
-      },
-      propsData:{
-        shows: show
-      }
-    }
-    )
-    expect(wrappers.html()).toContain("p")
   });
+  it('should find show casts service',async () => {
+    jest.mock('@/Service/api', () => ({
+      get: () => {
+      return Promise.resolve({
+        data: true
+      })
+    },
+  })); 
 
-  it('should find h3 tag', () => {
-    expect(wrapper.html()).toContain("h3")
-  });  */
+  const localWrapper=shallowMount(DetailsPage, {
+    localVue,
+    router,
+    propsData:{
+      shows: 1
+    }
+  }
+  )
+
+  const resp = await localWrapper.vm.getTvShowCasts();
+  expect(localWrapper.vm.tvShowCast).toBeDefined(); 
+
+  });
    it('Should search the showDetails properly when mounted', async () => {
     let mockedResponse = {
       "id": 2, "name": "Under the Dome", "rating": { "average": 6.6 },
       "image": { "medium": "https://static.tvmaze.com/uploads/images/medium_portrait/81/202627.jpg" }, "genres": ["Drama", "Science-Fiction", "Thriller"]
     }
     await wrapper.vm.getTvShowDetails();
-    console.log("shoe",wrapper.vm.showDetails)
-    expect(wrapper.vm.showDetails).toEqual(mockedResponse);
+    expect(wrapper.vm.showDetails).toBeDefined();
   }) 
 
   it('Should search the showDetails properly when mounted', async () => {
@@ -100,8 +72,7 @@ describe('DetailsPage.vue', () => {
             "medium": "https://static.tvmaze.com/uploads/images/medium_portrait/24/60864.jpg",}}]
     
     await wrapper.vm.getTvShowSeasons();
-    console.log("shoe",wrapper.vm.showSeason)
-    expect(wrapper.vm.showSeason).toEqual(mockedResponse);
+    expect(wrapper.vm.showSeason).toBeDefined();
   }) 
 
    
